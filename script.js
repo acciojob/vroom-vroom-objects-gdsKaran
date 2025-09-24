ac513e42b892d3165da4b1a0907669f792fcd6f5
Gagandeep Singh <142520245+gdsKaran@users.noreply.github.com>
2025-09-24 17:34:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 // Complete the js code
 function Car(make, model) {
-	this.make = make;
-	this.model =  model;
-
-	Car.prototype.getMakeModel = function() {
-    console.log(this.make + " " + this.model);
-  };
+  this.make = make;
+  this.model = model;
 }
+
+// Add method to Car prototype
+Car.prototype.getMakeModel = function () {
+  return this.make + " " + this.model;
+};
 
 function SportsCar(make, model, topSpeed) {
-	Car.call(this,make,model);
-	this.topSpeed = topSpeed;
-
-		SportsCar.prototype.getTopSpeed= function(){
-		console.log(this.topSpeed);
-	}
+  // Call Car constructor
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
 }
+
+// Inherit from Car
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// Add method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
