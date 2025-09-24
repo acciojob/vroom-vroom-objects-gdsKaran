c06d0b4a2aeb9aa96c0befca2edfb92e85d2a5dd
Gagandeep Singh <142520245+gdsKaran@users.noreply.github.com>
2025-09-24 17:07:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.car = make;
+	this.model =  model;
 
-function SportsCar(make, model, topSpeed) {}
+	getMakeModel(){
+		console.log(make + " " + model); 
+	}
+}
+
+function SportsCar(make, model, topSpeed) {
+	super(make,model);
+	this.topSpeed = topSpeed;
+
+	getTopSpeed(){
+		console.log(topSpeed);
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
